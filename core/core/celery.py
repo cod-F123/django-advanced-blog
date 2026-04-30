da57d3408715b139082d6bfcee0972bdee882760
mohammad <mohammadjavadsh801@gmail.com>
2026-04-30 18:21:42 +0330

    add some changes

--- core/core/celery.py
+++ core/core/celery.py
@@ -1,6 +1,6 @@
 from celery import Celery
 import os 
-# from accounts.tasks import sendEmail
+from accounts.tasks import sendEmail
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
@@ -10,6 +10,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-# @app.on_after_configure.connect
-# def setup_on_periodic_tasks(sender, **kwargs):
-#     sender.add_periodic_task(10, sendEmail.s(), name="send Email each 10 second")
+@app.on_after_configure.connect
+def setup_on_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10, sendEmail.s(), name="send Email each 10 second")
